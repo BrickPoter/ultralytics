1ff776e86753689aa63e8803c105f7907c274c0c
chenjun <cj1694450162@163.com>
2025-05-15 21:39:38 +0800

    修改task.py支持添加CBAM模块，增加模型配置文件

--- ultralytics/nn/tasks.py
+++ ultralytics/nn/tasks.py
@@ -68,8 +68,8 @@ from ultralytics.nn.modules import (
     YOLOEDetect,
     YOLOESegment,
     v10Detect,
-    SEBlock,
     C3SE,
+    CBAM,
 )
 from ultralytics.utils import DEFAULT_CFG_DICT, DEFAULT_CFG_KEYS, LOGGER, YAML, colorstr, emojis
 from ultralytics.utils.checks import check_requirements, check_suffix, check_yaml
@@ -1412,6 +1412,7 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
             C2fCIB,
             A2C2f,
             C3SE,
+            CBAM,
         }
     )
     repeat_modules = frozenset(  # modules with 'repeat' arguments
@@ -1460,7 +1461,14 @@ def parse_model(d, ch, verbose=True):  # model_dict, input_channels(3)
                 args[1] = make_divisible(min(args[1], max_channels // 2) * width, 8)
                 args[2] = int(max(round(min(args[2], max_channels // 2 // 32)) * width, 1) if args[2] > 1 else args[2])
 
-            args = [c1, c2, *args[1:]]
+            # args = [c1, c2, *args[1:]] 源码
+            # CBAM 只有c1参数，因此不需要c2参数
+            if m is CBAM:
+                args = [c1, *args[1:]]
+                pass
+            else:
+                args = [c1, c2, *args[1:]]
+
             if m in repeat_modules:
                 args.insert(2, n)  # number of repeats
                 n = 1
